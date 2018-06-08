ab9841bab74ca0f2728e18d7147f7aaf2c6c7751
cryptum <danilovbvip@hotmail.com>
2018-06-08 16:55:56 -0300

    2º Commit Atualização

---
+++ MQTT_Pub/src/main/java/DAO/LivroDao.java
@@ -0,0 +1,52 @@
+package DAO;
+
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import Control.Mosquitto_pub;
+import Model.LivroM;
+
+public class LivroDao {
+
+public static boolean InserirPessoa(LivroM liv) {
+		
+		String SQL = "INSERT INTO livraria.livro (autor, ano, titulo, genero) values (?, ?, ?, ?)"; 
+
+try {
+	PreparedStatement stmt = ConexaoBanco1.getConexaoMySQL().prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+
+	
+	stmt.setString(1, liv.getAutor());
+	stmt.setInt(2, liv.getAno());
+	stmt.setString(3, liv.getTitulo());
+	stmt.setString(4, liv.getGenero());
+	
+	 if(stmt.executeUpdate()>0){
+		
+		ResultSet result = stmt.getGeneratedKeys();
+		System.out.println("chegou aqui");
+		
+		if (result.next()) {
+			
+		    System.out.println("ID: " + result.getInt(1));
+		    
+		    Mosquitto_pub mosquittoPub = new Mosquitto_pub(Integer.toString(result.getInt(1)), "id", 1883, "localhost");
+		    
+		    if (!mosquittoPub.SendMessege()){
+		    	JOptionPane.showMessageDialog(null, "Erro no enviar mensagem 'LivroDAO'", "Sistema", JOptionPane.ERROR_MESSAGE);
+		    }		    
+		}		
+        return true;
+    } 
+} catch(SQLException ex) {
+	System.out.println("Erro: " + ex.getMessage());
+	return false;
+}
+return false;	
+}
+}
---
+++ MQTT_Pub/src/main/java/Model/LivroM.java
@@ -0,0 +1,51 @@
+package Model;
+
+public class LivroM {
+    private int Id;
+    private String Autor;
+    private int Ano;
+    private String Titulo;
+    private String Genero;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getAutor() {
+        return Autor;
+    }
+
+    public void setAutor(String Autor) {
+        this.Autor = Autor;
+    }
+
+    public int getAno() {
+        return Ano;
+    }
+
+    public void setAno(int Ano) {
+        this.Ano = Ano;
+    }
+
+    public String getTitulo() {
+        return Titulo;
+    }
+
+    public void setTitulo(String Titulo) {
+        this.Titulo = Titulo;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+    
+    
+}
