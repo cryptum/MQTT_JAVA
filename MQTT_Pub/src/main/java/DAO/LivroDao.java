d746453f791408eeaf3a865d0ce6e2a77cf20810
Leandro <Leandro@LAPTOP-MA11G6EE>
2018-06-11 16:48:09 -0300

    5.2º Commit Atualização Identações do pub corrigidas

--- MQTT_Pub/src/main/java/DAO/LivroDao.java
+++ MQTT_Pub/src/main/java/DAO/LivroDao.java
@@ -1,6 +1,5 @@
 package DAO;
 
-
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
@@ -13,40 +12,41 @@ import Model.LivroM;
 
 public class LivroDao {
 
-public static boolean InserirPessoa(LivroM liv) {
-		
-		String SQL = "INSERT INTO livraria.livro (autor, ano, titulo, genero) values (?, ?, ?, ?)"; 
+    public static boolean InserirPessoa(LivroM liv) {
 
-try {
-	PreparedStatement stmt = ConexaoBanco1.getConexaoMySQL().prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+        String sql = "INSERT INTO livraria.livro (autor, ano, titulo, genero) values (?, ?, ?, ?)"; 
 
-	
-	stmt.setString(1, liv.getAutor());
-	stmt.setInt(2, liv.getAno());
-	stmt.setString(3, liv.getTitulo());
-	stmt.setString(4, liv.getGenero());
-	
-	 if(stmt.executeUpdate()>0){
-		
-		ResultSet result = stmt.getGeneratedKeys();
-		System.out.println("chegou aqui");
-		
-		if (result.next()) {
-			
-		    System.out.println("ID: " + result.getInt(1));
-		    
-		    Mosquitto_pub mosquittoPub = new Mosquitto_pub(Integer.toString(result.getInt(1)), "id", 1883, "localhost");
-		    
-		    if (!mosquittoPub.SendMessege()){
-		    	JOptionPane.showMessageDialog(null, "Erro no enviar mensagem 'LivroDAO'", "Sistema", JOptionPane.ERROR_MESSAGE);
-		    }		    
-		}		
-        return true;
-    } 
-} catch(SQLException ex) {
-	System.out.println("Erro: " + ex.getMessage());
-	return false;
-}
-return false;	
-}
+        try {
+            PreparedStatement stmt = ConexaoBanco1.getConexaoMySQL().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setString(1, liv.getAutor());
+            stmt.setInt(2, liv.getAno());
+            stmt.setString(3, liv.getTitulo());
+            stmt.setString(4, liv.getGenero());
+
+
+            if(stmt.executeUpdate()>0){
+
+                ResultSet result = stmt.getGeneratedKeys();
+
+                if (result.next()) {
+
+                    System.out.println("ID: " + result.getInt(1));
+
+                    Mosquitto_pub mosquittoPub = new Mosquitto_pub(Integer.toString(result.getInt(1)), "id", 1883, "localhost");
+
+                    if (!mosquittoPub.SendMessege()){
+                        JOptionPane.showMessageDialog(null, "Erro no enviar mensagem 'LivroDAO'", "Sistema", JOptionPane.ERROR_MESSAGE);
+                    }		    
+                }	
+
+                return true;
+            } 
+        }
+        catch(SQLException ex) {
+                System.out.println("Erro: " + ex.getMessage());
+                return false;
+        }
+        return false;	
+    }
 }
